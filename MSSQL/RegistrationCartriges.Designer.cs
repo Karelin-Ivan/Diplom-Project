bdb466971effb339ee48abd06b85c949466834a5
KarelinIvan <i.karelin@bk.ru>
2024-05-18 21:54:06 +0300

    0.3.0

---
+++ MSSQL/RegistrationCartriges.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace MSSQL
+{
+    partial class FormRegCartrige
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxBarcode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxOrganization = new System.Windows.Forms.ComboBox();
+            this.comboBoxModel = new System.Windows.Forms.ComboBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.labelFormName = new System.Windows.Forms.Label();
+            this.buttonChanngeStyle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxBarcode
+            // 
+            this.textBoxBarcode.Enabled = false;
+            this.textBoxBarcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxBarcode.Location = new System.Drawing.Point(19, 75);
+            this.textBoxBarcode.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxBarcode.Name = "textBoxBarcode";
+            this.textBoxBarcode.Size = new System.Drawing.Size(337, 26);
+            this.textBoxBarcode.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Штрихкод";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(15, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Модель";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(15, 164);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Организация";
+            // 
+            // comboBoxOrganization
+            // 
+            this.comboBoxOrganization.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxOrganization.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxOrganization.FormattingEnabled = true;
+            this.comboBoxOrganization.Items.AddRange(new object[] {
+            "Новые фермы",
+            "ПензаМолИнвест"});
+            this.comboBoxOrganization.Location = new System.Drawing.Point(19, 189);
+            this.comboBoxOrganization.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxOrganization.Name = "comboBoxOrganization";
+            this.comboBoxOrganization.Size = new System.Drawing.Size(337, 28);
+            this.comboBoxOrganization.TabIndex = 8;
+            // 
+            // comboBoxModel
+            // 
+            this.comboBoxModel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxModel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBoxModel.FormattingEnabled = true;
+            this.comboBoxModel.Location = new System.Drawing.Point(19, 131);
+            this.comboBoxModel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxModel.Name = "comboBoxModel";
+            this.comboBoxModel.Size = new System.Drawing.Size(302, 28);
+            this.comboBoxModel.TabIndex = 9;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.BackColor = System.Drawing.Color.DarkGray;
+            this.buttonSave.FlatAppearance.BorderSize = 0;
+            this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSave.ForeColor = System.Drawing.Color.White;
+            this.buttonSave.Location = new System.Drawing.Point(116, 227);
+            this.buttonSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(120, 45);
+            this.buttonSave.TabIndex = 12;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = false;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // labelFormName
+            // 
+            this.labelFormName.AutoSize = true;
+            this.labelFormName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelFormName.Location = new System.Drawing.Point(14, 11);
+            this.labelFormName.Name = "labelFormName";
+            this.labelFormName.Size = new System.Drawing.Size(307, 29);
+            this.labelFormName.TabIndex = 13;
+            this.labelFormName.Text = "Добавление картриджа";
+            // 
+            // buttonChanngeStyle
+            // 
+            this.buttonChanngeStyle.Location = new System.Drawing.Point(324, 131);
+            this.buttonChanngeStyle.Name = "buttonChanngeStyle";
+            this.buttonChanngeStyle.Size = new System.Drawing.Size(32, 28);
+            this.buttonChanngeStyle.TabIndex = 14;
+            this.buttonChanngeStyle.Text = "R";
+            this.buttonChanngeStyle.UseVisualStyleBackColor = true;
+            this.buttonChanngeStyle.Click += new System.EventHandler(this.buttonChanngeStyle_Click);
+            // 
+            // FormRegCartrige
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 281);
+            this.Controls.Add(this.buttonChanngeStyle);
+            this.Controls.Add(this.labelFormName);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.comboBoxModel);
+            this.Controls.Add(this.comboBoxOrganization);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxBarcode);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormRegCartrige";
+            this.Text = "AddCartrige";
+            this.Load += new System.EventHandler(this.formAddCartrige_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxBarcode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxOrganization;
+        private System.Windows.Forms.ComboBox comboBoxModel;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label labelFormName;
+        private System.Windows.Forms.Button buttonChanngeStyle;
+    }
+}
